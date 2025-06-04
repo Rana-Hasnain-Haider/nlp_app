5ea18193470a56c332d4d5bf56538d8961a79732
Rana-Hasnain-Haider <ranahasnainhaider44@gmail.com>
2025-06-04 15:34:29 +0500

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from UIpages.transformer_segmenter import show_segmenter_ui
 def download_nltk_data():
     nltk.download('punkt')
     nltk.download('punkt_tab')
-    nltk.download('averaged_perceptron_tagger')
+    nltk.download('averaged_perceptron_tagger_eng')
     nltk.download('maxent_ne_chunker')
     nltk.download('words')
     nltk.download('stopwords')  
